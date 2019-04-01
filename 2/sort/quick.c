315ed0a803d8a72d6b2376b6927b043ba0170f49
Bartor <bartorkrajczyk@gmail.com>
2019-04-01 20:06:57 +0200

    quicksort naprawiony

--- 2/sort/quick.c
+++ 2/sort/quick.c
@@ -3,37 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int partition(void* data, int lo, int e_size, int hi, int (*comparator)(void*, void*), int* results) {
-	int i = lo - 1;
+void q(void* data, int l, int r, int e_size, int (*comparator)(void*, void*), int* results) {
+	if (l >= r) return;
+	int i = l;
+	int j = r;
 	
-	for (int j = lo; j <= hi - 1; j++) {
-		results[0]++;
-		if ((*comparator)(data + j*e_size, data + hi*e_size) <= 0) {
-			results[1]++;
+	void* pivot = malloc(e_size);
+	memcpy(pivot, data + i*e_size, e_size);
+	
+	while (1) {
+		results[0] += 2;
+		while((*comparator)(data + i*e_size, pivot) < 0) {
+			results[0]++;
 			i++;
-			void* temp = malloc(e_size);
-			memcpy(temp, data + i*e_size, e_size);
-			memcpy(data + i*e_size, data + j*e_size, e_size);
-			memcpy(data + j*e_size, temp, e_size);
-			free(temp);
 		}
+		while((*comparator)(pivot, data + j*e_size) < 0) {
+			results[0]++;
+			j--;
+		}
+		
+		if (i >= j) break;
+		
+		results[1]++;
+		void* temp = malloc(e_size);
+		memcpy(temp, data + i*e_size, e_size);
+		memcpy(data + i*e_size, data + j*e_size, e_size);
+		memcpy(data + j*e_size, temp, e_size);
+		free(temp);
 	}
-	results[1]++;
-	void* temp = malloc(e_size);
-	memcpy(temp, data + (i+1)*e_size, e_size);
-	memcpy(data + (i+1)*e_size, data + hi*e_size, e_size);
-	memcpy(data + hi*e_size, temp, e_size);
-	free(temp);
-
-	return i+1;
-}
-
-int q(void* data, int index, int e_size, int size, int (*comparator)(void*, void*), int* results) {
-	if (index < size) {
-		int m = partition(data, index, e_size, size, comparator, results);
-		q(data, index, e_size, m-1, comparator, results);
-		q(data, m+1, e_size, size, comparator, results);
-	}
+	
+	q(data, l, i-1, e_size, comparator, results);
+	q(data, j+1, r, e_size, comparator, results);
 }
 
 int* quick_sort(void* data, int e_size, int size, int (*comparator)(void*, void*)) {
@@ -41,7 +41,7 @@ int* quick_sort(void* data, int e_size, int size, int (*comparator)(void*, void*
 	results[0] = 0;
 	results[1] = 0;
 	
-	q(data, 0, e_size, size, comparator, results);
+	q(data, 0, size-1, e_size, comparator, results);
 	
 	return results;
 }
