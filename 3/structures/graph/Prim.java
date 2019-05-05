34ce5b0b0492e961d9d6f550279318334ffed0d6
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-05 22:43:35 +0200

    inne podejście do Prima

--- 3/structures/graph/Prim.java
+++ 3/structures/graph/Prim.java
@@ -3,43 +3,50 @@ package structures.graph;
 import structures.queue.BasicQueueElement;
 import structures.queue.HeapPriorityQueue;
 import structures.queue.PriorityQueueInterface;
+import structures.queue.QueueElementInterface;
 
 public class Prim {
-    private Integer[] parent;
-    private int[] keys;
     private UndirectedGraph graph;
 
     public Prim(UndirectedGraph graph) {
         this.graph = graph;
-        parent = new Integer[graph.getEdges().length];
-        keys = new int[graph.getEdges().length];
     }
 
-    public UndirectedGraph mst(int root) {
+    public UndirectedGraph mst() {
         UndirectedGraph g = new UndirectedGraph(graph.getEdges().length);
+
+        boolean[] mst = new boolean[graph.getEdges().length];
+        int[] key = new int[graph.getEdges().length];
 
         PriorityQueueInterface q = new HeapPriorityQueue();
         for (int i = 0; i < graph.getEdges().length; i++) {
-            keys[i] = Integer.MAX_VALUE;
+            if (i == 0) {
+                key[0] = 0;
+                mst[0] = true;
+            } else {
+                key[i] = Integer.MAX_VALUE;
+                mst[i] = false;
+            }
+            q.insert(new BasicQueueElement<Integer>(i, key[i]));
         }
-        keys[root] = 0;
-        for (int i = 0; i < graph.getEdges().length; i++) {
-            q.insert(new BasicQueueElement<Integer>(i, keys[i]));
-        }
-        parent[root] = null;
 
         while (!q.empty()) {
-            int u = (Integer) q.top().getValue();
-            for (GraphEdge e : graph.getEdges()[u]) {
-                if (q.contains(new BasicQueueElement<Integer>(e.from, 0)) && e.weight < keys[e.from]) {
-                    parent[e.from] = e.to;
-                    keys[e.from] = e.weight;
-                    q.priority(new BasicQueueElement<Integer>(e.from, keys[e.from]));
-                    g.addEdge(e);
+            QueueElementInterface e = q.pop();
+            int vertex = (Integer) e.getValue();
+
+            mst[vertex] = true;
+
+            for (GraphEdge edge : graph.getEdges()[vertex]) {
+                if (!mst[edge.to]) {
+                    if (key[edge.to] > edge.weight) {
+                        q.insert(new BasicQueueElement<Integer>(edge.to, edge.weight));
+                        g.addEdge(new GraphEdge(edge.from, edge.to, edge.weight));
+                        key[edge.to] = edge.weight;
+                    }
                 }
             }
-            q.pop();
         }
+
         return g;
     }
 }
