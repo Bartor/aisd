e9c04186ed62b1f44edee8dca9a9daab6cfe4ca4
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-06 12:17:31 +0200

    dodano uruchamianie dijkstry

---
+++ 3/DLauncher.java
@@ -0,0 +1,37 @@
+import structures.graph.Dijkstra;
+import structures.graph.DirectedGraph;
+import structures.graph.GraphEdge;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class DLauncher {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        in.nextLine();
+
+        DirectedGraph d = new DirectedGraph(n);
+
+        for (int i = 0; i < m ; i++) {
+            String input = in.nextLine();
+            String[] line = input.split(" ");
+            try {
+                d.addEdge(new GraphEdge(Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2])));
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+                return;
+            }
+        }
+
+        int start = in.nextInt();
+
+        Dijkstra di = new Dijkstra(d);
+        List<Integer>[] paths = di.shortestPath(start);
+
+        for (List<Integer> l : paths) System.out.println(l);
+    }
+}
