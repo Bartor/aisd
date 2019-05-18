a78f8dadf8258b177ac09adb93a76f0d2600e8ef
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-18 17:34:56 +0200

    działające splay

--- 4/tree/impl/SplayTree.java
+++ 4/tree/impl/SplayTree.java
@@ -2,23 +2,38 @@ package tree.impl;
 
 import tree.TreeInterface;
 import tree.structs.Node;
+import tree.structs.Stats;
 
 import java.util.Stack;
 
 public class SplayTree<T extends Comparable<T>> extends StringLoadingTree<T> implements TreeInterface<T> {
     private Node<T> root;
+    private Stats stats;
+
+    public SplayTree(Stats stats) {
+        this.stats = stats;
+    }
 
     @Override
     public void insert(T element) {
         Node<T> e = bstInsert(element);
+
+        stats.nodeComp++;
         if (e != null) {
-            while (e.getP() != null) splay(e);
+            stats.nodeComp++;
+            if (e.getP() != null) {
+                Node<T> p = e.getP();
+                while (p.getP() != null) {
+                    stats.nodeComp++;
+                    splay(p);
+                }
+            }
         }
     }
 
     @Override
     public void delete(T element) {
-        Node<T> e =bstDelete(element);
+        Node<T> e = bstDelete(element);
         if (e != null && e.getP() != null) {
             Node<T> p = e.getP();
             while (p.getP() != null) splay(p);
@@ -31,7 +46,7 @@ public class SplayTree<T extends Comparable<T>> extends StringLoadingTree<T> imp
         StringBuilder b = new StringBuilder();
 
         Stack<Node<T>> s = new Stack<>();
-        while(x != null || s.size() > 0) {
+        while (x != null || s.size() > 0) {
             while (x != null) {
                 s.push(x);
                 x = x.getL();
@@ -51,103 +66,131 @@ public class SplayTree<T extends Comparable<T>> extends StringLoadingTree<T> imp
     }
 
     private void splay(Node<T> n) {
-        Node<T> p = n.getP();
-        Node<T> gp = (p != null) ? p.getP() : null;
-        if (p != null && p == root) {
-            root = n;
-            n.setP(null);
-
-            if (n == p.getL()) {
-                p.setL(n.getR());
-                if (n.getR() != null) n.getR().setP(p);
-                n.setR(p);
-                p.setP(n);
-            } else {
-                p.setR(n.getL());
-                if (n.getL() != null) n.getL().setP(p);
-                n.setL(p);
-                p.setP(n);
-            }
-            return;
-        }
-        if (p != null && gp != null) {
-            Node<T> ggp = gp.getP();
-            if (ggp != null && ggp.getL() == gp) {
-                ggp.setL(n);
-                n.setP(ggp);
-            } else if (ggp != null && ggp.getR() == gp) {
-                ggp.setR(n);
-                n.setP(ggp);
-            } else {
+        Node<T> parent = n.getP();
+        stats.nodeComp++;
+        Node<T> gp = (parent != null) ? parent.getP() : null;
+        stats.nodeComp++;
+        if (parent != null) {
+            stats.nodeComp++;
+            if (parent == root) {
                 root = n;
                 n.setP(null);
-            }
 
-            if ((n == p.getL() && p == gp.getL()) || (n == p.getR() && p == gp.getR())) {
-                if (n == p.getL()) {
-                    Node<T> nr = n.getR();
-                    n.setR(p);
-                    p.setP(n);
-
-                    p.setL(nr);
-                    if (nr != null) nr.setP(p);
-
-                    Node<T> pr = p.getR();
-                    p.setR(gp);
-                    gp.setP(p);
-
-                    gp.setL(pr);
-                    if (pr != null) pr.setP(gp);
+                stats.nodeComp++;
+                if (n == parent.getL()) {
+                    parent.setL(n.getR());
+                    stats.nodeComp++;
+                    if (n.getR() != null) n.getR().setP(parent);
+                    n.setR(parent);
+                    parent.setP(n);
                 } else {
-                    Node<T> nl = n.getL();
-                    n.setL(p);
-                    p.setP(n);
-
-                    p.setR(nl);
-                    if (nl != null) nl.setP(p);
-
-                    Node<T> pl = p.getL();
-                    p.setL(gp);
-                    gp.setP(p);
-
-                    gp.setR(pl);
-                    if (pl != null) pl.setP(gp);
+                    parent.setR(n.getL());
+                    stats.nodeComp++;
+                    if (n.getL() != null) n.getL().setP(parent);
+                    n.setL(parent);
+                    parent.setP(n);
                 }
                 return;
             }
+        }
 
-            if (n == p.getL()) {
-                Node<T> nl = n.getR();
+        stats.nodeComp++;
+        if (parent != null) {
+            stats.nodeComp++;
+            if (gp != null) {
+                Node<T> ggp = gp.getP();
+                stats.nodeComp += 2;
+                if (ggp != null && ggp.getL() == gp) {
+                    ggp.setL(n);
+                    n.setP(ggp);
+                } else if (ggp != null && ggp.getR() == gp) {
+                    stats.nodeComp += 2;
+                    ggp.setR(n);
+                    n.setP(ggp);
+                } else {
+                    stats.nodeComp += 2;
+                    root = n;
+                    n.setP(null);
+                }
+
+                stats.nodeComp += 3;
+                if ((n == parent.getL() && parent == gp.getL()) || (n == parent.getR() && parent == gp.getR())) {
+                    stats.nodeComp++;
+                    if (n == parent.getL()) {
+                        Node<T> nr = n.getR();
+                        n.setR(parent);
+                        parent.setP(n);
+
+                        parent.setL(nr);
+                        stats.nodeComp++;
+                        if (nr != null) nr.setP(parent);
+
+                        Node<T> pr = parent.getR();
+                        parent.setR(gp);
+                        gp.setP(parent);
+
+                        gp.setL(pr);
+                        stats.nodeComp++;
+                        if (pr != null) pr.setP(gp);
+                    } else {
+                        Node<T> nl = n.getL();
+                        n.setL(parent);
+                        parent.setP(n);
+
+                        parent.setR(nl);
+                        stats.nodeComp++;
+                        if (nl != null) nl.setP(parent);
+
+                        Node<T> pl = parent.getL();
+                        parent.setL(gp);
+                        gp.setP(parent);
+
+                        gp.setR(pl);
+                        stats.nodeComp++;
+                        if (pl != null) pl.setP(gp);
+                    }
+                    return;
+                }
+
+                stats.nodeComp++;
+                if (n == parent.getL()) {
+                    Node<T> nl = n.getR();
+                    Node<T> nr = n.getL();
+
+                    n.setR(parent);
+                    parent.setP(n);
+
+                    n.setL(gp);
+                    gp.setP(n);
+
+                    parent.setL(nl);
+                    stats.nodeComp++;
+                    if (nl != null) nl.setP(parent);
+
+                    gp.setR(nr);
+                    stats.nodeComp++;
+                    if (nr != null) nr.setP(gp);
+
+                    return;
+                }
+
+                Node<T> nl = n.getL();
                 Node<T> nr = n.getR();
 
-                n.setR(p);
-                p.setP(n);
+                n.setL(parent);
+                parent.setP(n);
 
-                n.setL(gp);
+                n.setR(gp);
                 gp.setP(n);
 
-                p.setL(nl);
-                if (nl != null) nl.setP(p);
+                parent.setR(nl);
+                stats.nodeComp++;
+                if (nl != null) nl.setP(parent);
 
-                gp.setR(nr);
+                gp.setL(nr);
+                stats.nodeComp++;
                 if (nr != null) nr.setP(gp);
-                return;
             }
-
-            Node<T> nl = n.getL();
-            Node<T> nr = n.getR();
-
-            n.setL(p);
-            p.setP(n);
-
-            n.setR(gp);
-            gp.setP(n);
-
-            p.setR(nl);
-            if (nl != null) nl.setP(p);
-
-            gp.setL(nr);
-            if (nr != null) nr.setP(gp);
         }
     }
 
@@ -160,49 +203,76 @@ public class SplayTree<T extends Comparable<T>> extends StringLoadingTree<T> imp
     }
 
     private Node<T> bstLookUp(Node<T> e, T value) {
-        if (e == null || value.compareTo(e.getKey()) == 0) return e;
-        if (value.compareTo(e.getKey()) < 0) return bstLookUp(e.getL(), value);
-        else return bstLookUp(e.getR(), value);
+        while (e != null && value.compareTo(e.getKey()) != 0) {
+            stats.nodeComp += 2;
+            stats.keyComp++;
+            if (value.compareTo(e.getKey()) < 0) {
+                e = e.getL();
+            } else {
+                e = e.getR();
+            }
+        }
+        return e;
     }
 
     private Node<T> bstInsert(T element) {
-        Node<T> e = new Node<>(element);
-
         Node<T> y = null;
         Node<T> x = root;
 
+        Node<T> z = new Node<>(element);
+
         while (x != null) {
+            stats.nodeComp++;
             y = x;
-            x = e.compareTo(x) < 0 ? x.getL() : x.getR();
+            if (z.compareTo(x) < 0) {
+                x = x.getL();
+            } else {
+                x = x.getR();
+            }
         }
 
-        e.setP(y);
+        stats.nodeChange++;
+        z.setP(y);
+
+        stats.nodeComp++;
         if (y == null) {
-            root = e;
-        } else if (e.compareTo(y) < 0) {
-            y.setL(e);
+            stats.nodeChange++;
+            root = z;
+        } else if (z.compareTo(y) < 0) {
+            stats.nodeComp++;
+            stats.nodeChange++;
+            y.setL(z);
         } else {
-            y.setR(e);
+            stats.nodeComp++;
+            stats.nodeChange++;
+            y.setR(z);
         }
 
-        return e;
+        return z;
     }
 
     private Node<T> bstDelete(T element) {
         Node<T> z = bstLookUp(root, element);
+
         if (z != null) {
+            stats.nodeComp++;
             if (z.getL() == null) {
-                trans(z, z.getR());
+                return trans(z, z.getR());
             } else if (z.getR() == null) {
-                trans(z, z.getL());
+                stats.nodeComp++;
+                return trans(z, z.getL());
             } else {
+                stats.nodeComp++;
                 Node<T> y = min(z.getR());
+                stats.nodeComp++;
                 if (y.getP() != z) {
                     trans(y, y.getR());
+                    stats.nodeChange += 2;
                     y.setR(z.getR());
                     y.getR().setP(y);
                 }
                 trans(z, y);
+                stats.nodeChange += 2;
                 y.setL(z.getL());
                 y.getL().setP(y);
                 return y;
@@ -211,17 +281,27 @@ public class SplayTree<T extends Comparable<T>> extends StringLoadingTree<T> imp
         return null;
     }
 
-    private void trans(Node<T> u, Node<T> v) {
+    private Node<T> trans(Node<T> u, Node<T> v) {
+        stats.nodeComp++;
         if (u.getP() == null) {
+            stats.nodeChange++;
             root = v;
         } else if (u == u.getP().getL()) {
+            stats.nodeComp++;
+            stats.nodeChange++;
             u.getP().setL(v);
         } else {
+            stats.nodeComp++;
+            stats.nodeChange++;
             u.getP().setR(v);
         }
+        stats.nodeComp++;
         if (v != null) {
+            stats.nodeChange++;
             v.setP(u.getP());
         }
+
+        return v;
     }
 
     private Node<T> min(Node<T> x) {
