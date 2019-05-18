cf93629561039ba766d500174e98c4542f0fca2d
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-18 11:14:41 +0200

    statystyki w BST

--- 4/tree/impl/BST.java
+++ 4/tree/impl/BST.java
@@ -2,13 +2,16 @@ package tree.impl;
 
 import tree.TreeInterface;
 import tree.structs.Node;
+import tree.structs.Stats;
 
 import java.util.Stack;
 
 public class BST<T extends Comparable<T>> extends StringLoadingTree<T> implements TreeInterface<T> {
     private Node<T> root;
+    private Stats stats;
 
-    public BST() {
+    public BST(Stats stats) {
+        this.stats = stats;
         root = null;
     }
 
@@ -20,16 +23,24 @@ public class BST<T extends Comparable<T>> extends StringLoadingTree<T> implement
         Node<T> x = root;
 
         while (x != null) {
+            stats.nodeComp++;
             y = x;
+            stats.keyComp++;
             x = e.compareTo(x) < 0 ? x.getL() : x.getR();
         }
 
         e.setP(y);
+        stats.nodeChange++;
         if (y == null) {
+            stats.nodeComp++;
             root = e;
         } else if (e.compareTo(y) < 0) {
+            stats.nodeComp += 2;
+            stats.nodeChange++;
             y.setL(e);
         } else {
+            stats.nodeComp += 2;
+            stats.nodeChange++;
             y.setR(e);
         }
     }
@@ -37,19 +48,26 @@ public class BST<T extends Comparable<T>> extends StringLoadingTree<T> implement
     @Override
     public void delete(T element) {
         Node<T> z = lookUpNode(root, element);
+        stats.nodeComp++;
         if (z != null) {
             if (z.getL() == null) {
+                stats.keyComp++;
                 trans(z, z.getR());
             } else if (z.getR() == null) {
+                stats.keyComp += 2;
                 trans(z, z.getL());
             } else {
+                stats.keyComp += 2;
                 Node<T> y = min(z.getR());
+                stats.nodeComp++;
                 if (y.getP() != z) {
                     trans(y, y.getR());
+                    stats.nodeChange+=2;
                     y.setR(z.getR());
                     y.getR().setP(y);
                 }
                 trans(z, y);
+                stats.nodeChange+=2;
                 y.setL(z.getL());
                 y.getL().setP(y);
             }
@@ -68,7 +86,9 @@ public class BST<T extends Comparable<T>> extends StringLoadingTree<T> implement
 
         Stack<Node<T>> s = new Stack<>();
         while(x != null || s.size() > 0) {
+            stats.nodeComp++;
             while (x != null) {
+                stats.nodeComp++;
                 s.push(x);
                 x = x.getL();
             }
@@ -82,26 +102,42 @@ public class BST<T extends Comparable<T>> extends StringLoadingTree<T> implement
     }
 
     private Node<T> lookUpNode(Node<T> e, T value) {
-        if (e == null || value.compareTo(e.getKey()) == 0) return e;
-        if (value.compareTo(e.getKey()) < 0) return lookUpNode(e.getL(), value);
-        else return lookUpNode(e.getR(), value);
+        while (e != null && value.compareTo(e.getKey()) != 0) {
+            stats.nodeComp++;
+            stats.keyComp += 2;
+            if (value.compareTo(e.getKey()) < 0) {
+                e = e.getL();
+            } else {
+                e = e.getR();
+            }
+        }
+        return e;
     }
 
     private void trans(Node<T> u, Node<T> v) {
+        stats.nodeComp++;
         if (u.getP() == null) {
             root = v;
         } else if (u == u.getP().getL()) {
+            stats.nodeComp++;
+            stats.nodeChange++;
             u.getP().setL(v);
         } else {
+            stats.nodeComp++;
+            stats.nodeChange++;
             u.getP().setR(v);
         }
+        stats.nodeComp++;
         if (v != null) {
+            stats.nodeChange++;
             v.setP(u.getP());
         }
     }
 
     private Node<T> min(Node<T> x) {
         while (x.getL() != null) {
+            stats.nodeComp++;
+            stats.nodeChange++;
             x = x.getL();
         }
         return x;
---
+++ 4/tree/structs/Stats.java
@@ -0,0 +1,7 @@
+package tree.structs;
+
+public class Stats {
+    public int keyComp = 0;
+    public int nodeComp = 0;
+    public int nodeChange = 0;
+}
