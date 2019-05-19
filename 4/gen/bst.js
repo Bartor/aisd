c57c232909a4d1576b7a22b442ffeac8bb9b9f25
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-19 21:05:10 +0200

    dodano generowanie idealnych danych dla bst wraz z przykładowym odpaleniem

---
+++ 4/gen/bst.js
@@ -0,0 +1,30 @@
+function numToString(n, limit) {
+    const alphabet = "abcdefghijklmnoupqrstuvwxyz";
+    let pos = 0;
+    while (n > limit) {
+        n -= limit;
+        pos++;
+    }
+    if (alphabet[pos] === undefined) throw "This limit is too low";
+    return Array(n).fill(alphabet[pos]).join("");
+}
+
+function generate(n) {
+    let res = [];
+    let arrays = [n];
+    let newArrays = [];
+    while (res.length != n.length) {
+        for (let a of arrays) {
+            let mid = Math.floor(a.length/2);
+            if (a.length == 0) continue;
+            res.push(a[mid]);
+            newArrays.push(a.slice(mid+1));
+            newArrays.push(a.slice(0, mid));
+        }
+        [arrays, newArrays] = [newArrays, []];
+    }
+    return res;
+}
+
+//generate 10000 lines of example data
+console.log(generate(Array(10000).fill(0).map((e, i) => i)).map(e => numToString(e, 1000)).join("\n"));
