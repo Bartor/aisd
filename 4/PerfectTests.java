55cc481a05ea28b4adaf508f842bf067ce03e3d7
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-19 23:40:08 +0200

    zbieranie danych idealnych

--- 4/PerfectTests.java
+++ 4/PerfectTests.java
@@ -10,38 +10,58 @@ import java.io.File;
 import java.util.List;
 
 public class PerfectTests {
+    private static final int ITERS = 20;
+
     public static void main(String[] args) {
         File bstf = new File("gen/pbst2.txt");
 
         TreeInterface[] trees = new TreeInterface[3];
         File[] files = {bstf};
 
-        for (File f : files) {
-            trees[2] = new RBT<String>(new Stats());
-            trees[1] = new SplayTree<String>(new Stats());
-            trees[0] = new BST<String>(new Stats());
+        long[][][] res = new long[files.length][trees.length][3];
+        int fc, tc;
 
-            for (TreeInterface t : trees) {
-                System.out.println(f + " : " + t);
+        for (int i = 0; i < ITERS; i++)  {
+            fc = 0;
+            for (File f : files) {
+                tc = 0;
+                trees[2] = new RBT<String>(new Stats());
+                trees[1] = new SplayTree<String>(new Stats());
+                trees[0] = new BST<String>(new Stats());
 
-                DecoratedWordLoader d = new DecoratedWordLoader(new BasicDecorator());
-                List<String> words = d.load(bstf);
+                for (TreeInterface t : trees) {
+                    DecoratedWordLoader d = new DecoratedWordLoader(new BasicDecorator());
+                    List<String> words = d.load(f);
 
-                long time = System.nanoTime();
-                for (String word : words) {
-                    t.insert(word);
+                    long time = System.nanoTime();
+                    for (String word : words) {
+                        t.insert(word);
+                    }
+                    res[fc][tc][0] += (System.nanoTime() - time)/ITERS;
+                    time = System.nanoTime();
+                    for (String word : words) {
+                        t.search(word);
+                    }
+                    res[fc][tc][1] += (System.nanoTime() - time)/ITERS;
+                    time = System.nanoTime();
+                    for (String word : words) {
+                        t.delete(word);
+                    }
+                    res[fc][tc][2] += (System.nanoTime() - time)/ITERS;
+                    tc++;
                 }
-                System.out.println("insert done in " + (System.nanoTime() - time)/1000000f);
-                time = System.nanoTime();
-                for (String word : words) {
-                    t.search(word);
+
+                fc++;
+            }
+        }
+
+        for (long[][] a : res) {
+            System.out.println();
+            for (long[] b : a) {
+                for (long c : b) {
+                    System.out.print(c/1000000f + " ");
                 }
-                System.out.println("search done in " + (System.nanoTime() - time)/1000000f);
-                time = System.nanoTime();
-                for (String word : words) {
-                    t.delete(word);
-                }
-                System.out.println("delete done in " + (System.nanoTime() - time)/1000000f);
+                System.out.println(" ms");
             }
         }
     }
