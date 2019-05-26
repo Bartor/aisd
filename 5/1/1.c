e3719254885fd8b788d63b22fde299743c621ea8
Bartor <bartorkrajczyk@gmail.com>
2019-05-26 22:51:47 +0200

    poprawne wyniki dla 100

--- 5/1/1.c
+++ 5/1/1.c
@@ -89,7 +89,7 @@ int fordFulkerson(int** graph, int size, int start, int end, int* paths) {
 
 int main(void) {
 	int SIZE = 16;
-	int REPS = 1;
+	int REPS = 100;
 	srand(time(NULL));
 	printf("i, flow, time, paths\n");
 	for (int i = 1; i <= SIZE; i++) {
